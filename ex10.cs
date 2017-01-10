d42894d7ad63c465d114918496b3bbc4a7c5b5a7
Andrew Volokh <laturo.volokh@yandex.ru>
2017-01-10 21:40:44 +0400

    Create ex10.cs

---
+++ ex10.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace foresr
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Random r = new Random();
+			int size=int.Parse(Console.ReadLine());
+			int[,] array = new int[size, size];
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					array[i, j] = r.Next(0, 9);
+				}
+			}
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < i+1; j++)
+				{
+					Console.Write("{0} ", array[i, j]);
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
